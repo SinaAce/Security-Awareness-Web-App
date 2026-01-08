9bc000e9262dfa956b4655e44b653328802496bf
Sina Mohammadi <160613931+SinaAce@users.noreply.github.com>
2026-01-08 07:43:19 +0330

    feat(pwa): enable Progressive Web App functionality with service worker and manifest

--- security-awareness-web-app/vite.config.js
+++ security-awareness-web-app/vite.config.js
@@ -1,8 +1,32 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: { host: true, port: 5173 },
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Shilda Security App",
+        short_name: "Shilda",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#1B262C",
+        theme_color: "#028673",
+        icons: [
+          {
+            src: "icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
